9c9f346b3e9a492e18f85d01a29e974a4e044ad7
Mathieu WOLFF <61269077+MatW00@users.noreply.github.com>
2020-05-20 12:26:38 +0200

    add save in src/Form and updated _form + trans

--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,7 +13,12 @@ class CategoryType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
+            ->add('title', null, [
+                "label" => "form.title.category",
+            ])
+
+            // Ajout du submit
+            ->add('save', SubmitType::class, ['label' => 'save']);
         ;
     }
 
