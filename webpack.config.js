1b1a7405e4690b60f71a9e4e8aed383a2995c808
Julie Lacresse <61269042+leiju91@users.noreply.github.com>
2020-05-22 11:28:19 +0200

    webpack + erreur moustache dans scss

--- webpack.config.js
+++ webpack.config.js
@@ -23,13 +23,14 @@ Encore
    * Each entry will result in one JavaScript file (e.g. app.js)
    * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
    */
-  .addEntry('app', './assets/js/app.js')
-  .addEntry('admin', './assets/js/admin.js')
-  .addEntry('barre', './assets/js/barre.js')
-  .addEntry('detail', './assets/js/detail.js')
-  .addEntry('merci', './assets/js/merci.js')
-  .addEntry('contact', './assets/js/contact.js')
-  .addEntry('mentions-legales', './assets/js/mentions-legales.js')
+  .addEntry("app", "./assets/js/app.js")
+  .addEntry("admin", "./assets/js/admin.js")
+  .addEntry("barre", "./assets/js/barre.js")
+  .addEntry("detail", "./assets/js/detail.js")
+  .addEntry("merci", "./assets/js/merci.js")
+  .addEntry("contact", "./assets/js/contact.js")
+  .addEntry("mentions-legales", "./assets/js/mentions-legales.js")
+  .addEntry("generic", "./assets/js/generic.js")
 
   //.addEntry('page1', './assets/js/page1.js')
   //.addEntry('page2', './assets/js/page2.js')
